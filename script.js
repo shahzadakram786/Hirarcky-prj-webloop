cacd0834b469954574273971071bd7fb8708f584
shahzad akram <alyakram786@gmail.com>
2024-02-09 14:42:11 +0500

    in console work has done

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const deta = {
                 {
                     name: "back-end",
                     developers: [{
-                        name: "sheraz",
+                        name: "Asad",
                         skill: "Nodejs/php/blockchan"
                     }, {
                         name: "Nasir",
@@ -67,25 +67,111 @@ const deta = {
 }
 
 
-for (let i = 0; i < deta.length; i++) {
-    // let Data = deta[i];
-    console.log("jelo")
+// for (let i = 0; i < deta.length; i++) {
+//     let Data = deta[i];
 
-    let Ceo = document.getElementById("Ceo");
-    Ceo.addEventListener("click", () => {
-        console.log(deta.name);
-    })
+console.log("jelo")
 
-    console.log("jelo")
+let Ceo = document.getElementById("Ceo");
+Ceo.addEventListener("click", () => {
+    console.log(deta.name);
+})
 
-
-    // now is for manager button
-
-    let Managr = document.getElementById("manger");
-    Managr.addEventListener("click", () => {
-        console.log(data[i].hir.managers.name)
-
-    });
+console.log("jelo")
 
 
-}
+// now is for manager button
+
+const Man = document.getElementById("manger");
+Man.addEventListener("click", () => {
+    console.log(deta.hir.managers.name, '-', 'age :', deta.hir.managers.age)
+
+});
+
+
+
+// now evenlistener for front adn back
+
+
+const Front = document.getElementById("Frontend");
+
+Front.addEventListener("click", () => {
+    console.log(deta.hir.subhir1.types[0].name);
+
+})
+
+
+// for front developers 
+
+const FrntDev = document.getElementById("FrontDev");
+FrntDev.addEventListener("click", () => {
+    const FrontDev = deta.hir.subhir1.types[0].developers;
+
+    for (let i = 0; i < FrontDev.length; i++) {
+        console.log(FrontDev[i].name);
+    }
+});
+
+
+// for frontend interns 
+const frnt_intrns = document.getElementById("FrontInters");
+frnt_intrns.addEventListener("click", () => {
+    const frntIntrns = deta.hir.subhir2.Interns;
+    for (let i = 0; i < frntIntrns.length; i++) {
+        console.log(frntIntrns[i].name);
+    }
+
+
+});
+
+
+
+
+
+
+// const backDvlpr = deta.hir.subhir1.types[1].developers;
+
+// for (let i = 0; i < backDvlpr.length; i++) {
+// console.log(backDvlpr[i].name);
+// }
+
+
+
+
+
+
+// for back end
+const Back = document.getElementById("Back-End");
+
+Back.addEventListener("click", () => {
+
+    console.log(deta.hir.subhir1.types[1].name);
+
+});
+
+
+// for backdevelopers
+
+const bckDev = document.getElementById("bckDev");
+bckDev.addEventListener("click", () => {
+    const backDvlpr = deta.hir.subhir1.types[1].developers;
+    for (let i = 0; i < backDvlpr.length; i++) {
+        console.log(backDvlpr[i].name);
+
+
+    }
+});
+
+const bckintrns = document.getElementById("bckIntrns");
+bckintrns.addEventListener("click", () => {
+
+    const backIntrns = deta.hir.subhir1.types[1].developers;
+    for (let i = 0; i < backDvlpr.length; i++) {
+        console.log(backDvlpr[i].name);
+
+
+    }
+
+
+});
+// })
